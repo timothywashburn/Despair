a702973dfcc7e8bfd37332a9a6c117da310e9cb9
KyroKrypt <kyrokrypt@gmail.com>
2023-01-29 00:28:54 -0800

    catch error

--- src/main/java/dev/kyro/despair/controllers/PureTracker.java
+++ src/main/java/dev/kyro/despair/controllers/PureTracker.java
@@ -1,10 +1,8 @@
 package dev.kyro.despair.controllers;
 
 import dev.kyro.despair.exceptions.InvalidAPIKeyException;
-import dev.kyro.despair.exceptions.LookedUpNameRecentlyException;
 import dev.kyro.despair.exceptions.NoAPIKeyException;
 import dev.kyro.despair.firestore.Config;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.UUID;
@@ -26,19 +24,17 @@ public class PureTracker extends Thread {
 				requestData = HypixelAPIManager.request(UUID.fromString(Config.INSTANCE.PURE_ALT_UUID));
 			} catch(Exception exception) {
 				if(exception instanceof NoAPIKeyException) {
-					System.out.println("No api key set");
+					System.out.println("[Pure Tracker] No api key set");
 				} else if(exception instanceof InvalidAPIKeyException) {
-					System.out.println("Invalid api key");
-				} else if(exception instanceof LookedUpNameRecentlyException) {
-					System.out.println("That name was already looked up recently. Use the player's uuid instead or wait a minute");
+					System.out.println("[Pure Tracker] Invalid api key");
 				}
 				return;
 			}
 			try {
 				hypixelPlayer = new HypixelPlayer(requestData);
 				lastUpdate = System.currentTimeMillis();
-			} catch(JSONException ignored) {
-				System.out.println("Invalid player");
+			} catch(Exception ignored) {
+				System.out.println("[Pure Tracker] Invalid player");
 				return;
 			}
 
