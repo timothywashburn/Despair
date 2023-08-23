f90664ed4fe1df014695b950f754d6e62ee3d55d
Timothy <timothyrwashburn@gmail.com>
2023-08-22 18:34:18 -0700

    remove actual google key

---
+++ src/main/java/dev/kyro/despair/misc/Variables.java
@@ -0,0 +1,7 @@
+package dev.kyro.despair.misc;
+
+public class Variables {
+	public static String DISCORD_BOT_TOKEN = "INSERT_BOT_TOKEN_HERE";
+//	This only needs to be modified if multiple bots are being hosted off of same firestore database
+	public static String FIRESTORE_COLLECTION = "despair";
+}
